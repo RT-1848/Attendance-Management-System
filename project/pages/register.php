8be393902dfe48c27ea9baacd20e5ef018e0fe81
Yeb2b <yeb2b@mtmail.mtsu.edu>
2025-04-25 14:39:03 -0500

    Add files via upload

--- project/pages/register.php
+++ project/pages/register.php
@@ -120,6 +120,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" id="password" name="password" required>
+                    <!-- adding a picture to toggle password display -->
+                    <img src=
+                    "https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png"
+                     width="5%"
+                     height="5%"
+                     style=
+                         "display: inline; 
+                         margin-left: 93%;
+                         transform: translateY(-30px);
+                         vertical-align: middle;
+                         cursor: pointer"
+                     id="togglePassword">
                 </div>
                 <!-- Drop down for role selection -->
                 <div class="form-group">
@@ -135,5 +147,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <p>Already have an account? <a href="index.php?page=login">Login here</a></p>
         </div>
     </div>
+    <script>
+        // get the password input and toggle button
+        const togglePassword = document.querySelector('#togglePassword');
+        const password = document.querySelector('#password');
+        
+        // click event listener to toggle password
+        togglePassword.addEventListener('click', function() {
+            // if it is a password change to text and vice versa
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            
+            // change the eye icon
+            this.src = type === 'password' 
+                ? 'https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png'
+                : 'https://media.geeksforgeeks.org/wp-content/uploads/20210917150049/eyeslash.png';
+        });
+    </script>
 </body>
 </html> 
